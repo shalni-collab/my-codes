6bd40a186a24237471135b3f28a71cf2a218de99
shalni-collab <shalniasb121@gmail.com>
2025-01-24 11:52:09 +0530

    ly

--- just-main/just-main/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from blog import views
-urlpatterns = [
-    path('',views.blog,name="blog")
-    
-]
--- just-main/just-main/universe/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UniverseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'universe'
